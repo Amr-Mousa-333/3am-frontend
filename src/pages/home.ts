f1ccd7325f79fc938f71d81979a5857de563dc7c
Aya mohamed <aya667015@gmail.com>
2026-02-25 02:31:20 +0200

    gallary

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,6 +1,5 @@
 import { View } from "@lib/view";
 import { HomeHeroSection } from "@sections/home/hero";
-import { VideoGallery } from "@components/videoGallery";
 
 
 export class HomePage extends View<"section"> {
@@ -10,14 +9,7 @@ export class HomePage extends View<"section"> {
 
 	render(): DocumentFragment {
 		return this.tpl`
-			${new HomeHeroSection()}${new VideoGallery({
-    headline: "Get the highlights.",
-    slides: [
-        { src: "/assets/video1.mp4", caption: "Capture life in stunning detail." },
-        { src: "/assets/video2.mp4", caption: "Simply powerful." },
-        { src: "/assets/video3.mp4", caption: "Pro camera system. Simplified." },
-    ],
-})}
+			${new HomeHeroSection()}
 			<p style='margin-top: 1000px'>Hello</pstyle>
 		`;
 	}
